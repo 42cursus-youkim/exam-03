76ac68bcac9e3e5d36b52a62c07c5f2759264a79
scarf <greenscarf005@gmail.com>
2022-05-03 12:59:10 +0900

    inital? commit

--- micro_paint/micro_paint.c
+++ micro_paint/micro_paint.c
@@ -1,181 +1,92 @@
-#include <stdio.h>
+#include <string.h>
 #include <unistd.h>
+#include <stdio.h>
 #include <stdlib.h>
 
-typedef struct	s_zone
-{
-	int		width;
-	int		height;
-	char	background;
-} 				t_zone;
+#define ERR_ARG "Error: argument\n"
+#define ERR_OP "Error: Operation file corrupted\n"
+#define ZERO 0.000000
 
-typedef struct	s_shape
-{
-	char	type;
-	float	x;
-	float	y;
-	float	width;
-	float	height;
-	char	color;
-	struct s_shape	*next;
-}				t_shape;
+typedef enum { OUT, IN, EDGE } tile_t;
 
-int
-	ft_strlen(char const *str)
-{
-	int	i;
-
-	i = 0;
-	while (str[i])
-		i++;
-	return (i);
+int str_len(char *str) {
+  if (!str)
+    return 0;
+  int i = 0;
+  while (str[i])
+    i++;
+  return i;
 }
 
-int
-	str_error(char const *str, int ret)
-{
-	write(1, str, ft_strlen(str));
-	return (ret);
+int str_err(char *str) {
+  write(1, str, str_len(str));
+  return 1;
 }
 
-int
-	clear_all(FILE *file, char *drawing)
-{
-	fclose(file);
-	if (drawing)
-		free(drawing);
-	return (1);
+void cleanup(FILE *stream, char *grid) {
+  free(grid);
+  fclose(stream);
 }
 
-int
-	check_zone(t_zone *zone)
-{
-	return (zone->width > 0 && zone->width <= 300
-			&& zone->height > 0 && zone->height <= 300);
+tile_t get_tile_status(float x, float y, float rx, float rw, float ry, float rh) {
+  float rx2 = rx + rw;
+  float ry2 = ry + rh;
+  const float E = 1.000000;
+  if ((rx + E <= x && x <= rx2 - E) && (ry + E <= y && y <= ry2 - E))
+    return IN;
+  else if ((rx <= x && x <= rx2) && (ry <= y && y <= ry2))
+    return EDGE;
+  return OUT;
 }
 
-int
-	check_shape(t_shape *shape)
-{
-	return (shape->width > 0.00000000 && shape->height > 0.00000000
-			&& (shape->type == 'r' || shape->type == 'R'));
-}
+int main(int ac, char *av[]) {
+  FILE *stream;
+  int bg_width, bg_height;
+  char bg_char;
+  char *grid;
+  int res_len;
 
-int
-	get_zone(FILE *file, t_zone *zone)
-{
-	int scan_ret;
+//args
+  if (ac != 2)
+    return str_err(ERR_ARG);
 
-	if ((scan_ret = fscanf(file, "%d %d %c\n", &zone->width, &zone->height, &zone->background)) != 3)
-		return (0);
-	if (!check_zone(zone))
-		return (0);
-	if (scan_ret == -1)
-		return (0);
-	return (1);
-}
+//grid_setup
+  if (!(stream = fopen(av[1], "r"))
+      || (fscanf(stream, "%d %d %c\n", &bg_width, &bg_height, &bg_char) != 3)
+      || (!(0 < bg_width && bg_width <= 300) || !(0 < bg_height && bg_height <= 300))
+      || (!(grid = malloc(bg_height * bg_width))))
+    return str_err(ERR_OP);
+  memset(grid, bg_char, bg_width * bg_height);
 
-char
-	*paint_background(t_zone *zone)
-{
-	char	*drawing;
-	int		i;
+//grid_write
+  char rtype, rchar;
+  float rx, ry, rw, rh;
+  while ((res_len = fscanf(stream, "%c %f %f %f %f %c\n",
+          &rtype, &rx, &ry, &rw, &rh, &rchar)) == 6) {
+    // error check
+    if (rw <= ZERO || rh <= ZERO || !(rtype == 'r' || rtype == 'R'))
+      break;
+    // write to grid
+    for (int y = 0; y < bg_height; y++) {
+      for (int x = 0; x < bg_width; x++) {
+        tile_t tile = get_tile_status(x, y, rx, rw, ry, rh);
+        if (tile == EDGE || (tile == IN && rtype == 'R'))
+          grid[y * bg_width + x] = rchar;
+      }
+    }
+  }
+  if (res_len != EOF) {
+    cleanup(stream, grid);
+    return str_err(ERR_OP);
+  }
 
-	if (!(drawing = (char*)malloc(sizeof(*drawing) * (zone->width * zone->height))))
-		return (NULL);
-	i = 0;
-	while (i < zone->width * zone->height)
-		drawing[i++] = zone->background;
-	return (drawing);
-}
+// grid_print
+  for (int y = 0; y < bg_height; y++) {
+    write(1, grid + y * bg_width, bg_width);
+    write(1, "\n", 1);
+  }
 
-int
-	in_rectangle(float x, float y, t_shape *shape)
-{
-	if (((x < shape->x || (shape->x + shape->width < x))
-		|| (y < shape->y)) || (shape->y + shape->height < y))
-		return (0);
-	if (((x - shape->x < 1.00000000) || ((shape->x + shape->width) - x < 1.00000000)) ||
-	((y - shape->y < 1.00000000 || ((shape->y + shape->height) - y < 1.00000000))))
-		return (2);
-	return (1);
-}
-
-void
-	draw_shape(char **drawing, t_shape *shape, t_zone *zone)
-{
-	int	i;
-	int	j;
-	int	ret;
-
-	i = 0;
-	while (i < zone->height)
-	{
-		j = 0;
-		while (j< zone->width)
-		{
-			ret = in_rectangle(j, i, shape);
-			if ((shape->type == 'r' && ret == 2)
-				|| (shape->type == 'R' && ret))
-				(*drawing)[(i * zone->width) + j] = shape->color;
-			j++;
-		}
-		i++;
-	}
-}
-
-int
-	draw_shapes(FILE *file, char **drawing, t_zone *zone)
-{
-	t_shape	tmp;
-	int		scan_ret;
-
-	while ((scan_ret = fscanf(file, "%c %f %f %f %f %c\n", &tmp.type, &tmp.x, &tmp.y, &tmp.width, &tmp.height, &tmp.color)) == 6)
-	{
-		if (!check_shape(&tmp))
-			return (0);
-		draw_shape(drawing, &tmp, zone);
-	}
-	if (scan_ret != -1)
-		return (0);
-	return (1);
-}
-
-void
-	draw_drawing(char *drawing, t_zone *zone)
-{
-	int	i;
-
-	i = 0;
-	while (i < zone->height)
-	{
-		write(1, drawing + (i * zone->width), zone->width);
-		write(1, "\n", 1);
-		i++;
-	}
-}
-
-int
-	main(int argc, char **argv)
-{
-	t_zone	zone;
-	char	*drawing;
-	FILE	*file;
-
-	zone.width = 0;
-	zone.height = 0;
-	zone.background = 0;
-	if (argc != 2)
-		return (str_error("Error: argument\n", 1));
-	if (!(file = fopen(argv[1], "r")))
-		return (str_error("Error: Operation file corrupted\n", 1));
-	if (!get_zone(file, &zone))
-		return (clear_all(file, NULL) && str_error("Error: Operation file corrupted\n", 1));
-	if (!(drawing = paint_background(&zone)))
-		return (clear_all(file, NULL) && str_error("Error: malloc failed :)\n", 1));
-	if (!draw_shapes(file, &drawing, &zone))
-		return (clear_all(file, drawing) && str_error("Error: Operation file corrupted\n", 1));
-	draw_drawing(drawing, &zone);
-	clear_all(file, drawing);
-	return (0);
+//cleanup
+  cleanup(stream, grid);
+  return 0;
 }
--- mini_paint/mini_paint.c
+++ mini_paint/mini_paint.c
@@ -1,162 +1,95 @@
-#include <stdio.h>
+#include <string.h>
 #include <unistd.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 
-typedef struct	s_zone
-{
-	int		width;
-	int		height;
-	char	background;
-}				t_zone;
+#define ERR_ARG "Error: argument\n"
+#define ERR_OP "Error: Operation file corrupted\n"
+#define ZERO 0.000000
 
-typedef struct	s_shape
-{
-	char	type;
-	float	x;
-	float	y;
-	float	radius;
-	char	color;
-}				t_shape;
+typedef enum { OUT, IN, EDGE } tile_t;
 
-int
-	ft_strlen(char const *str)
-{
-	int	i;
-
-	i = 0;
-	while (str[i])
-		i++;
-	return (i);
+int str_len(char *str) {
+  if (!str)
+    return 0;
+  int i = 0;
+  while (str[i])
+    i++;
+  return i;
 }
 
-char
-	*get_zone(FILE *file, t_zone *zone)
-{
-	int		i;
-	char	*tmp;
-
-	if (fscanf(file, "%d %d %c\n", &zone->width, &zone->height, &zone->background) != 3)
-		return (NULL);
-	if (zone->width <= 0 || zone->width > 300 || zone->height <= 0 || zone->height > 300)
-		return (NULL);
-	if (!(tmp = (char*)malloc(sizeof(*tmp) * (zone->width * zone->height))))
-		return (NULL);
-	i = 0;
-	while (i < zone->width * zone->height)
-		tmp[i++] = zone->background;
-	return (tmp);
+int str_err(char *str) {
+  write(1, str, str_len(str));
+  return 1;
 }
 
-int
-	in_circle(float x, float y, t_shape *shape)
-{
-	float	distance;
-
-	distance = sqrtf(powf(x - shape->x, 2.) + powf(y - shape->y, 2.));
-	if (distance <= shape->radius)
-	{
-		if ((shape->radius - distance) < 1.00000000)
-			return (2);
-		return (1);
-	}
-	return (0);
+void cleanup(FILE *stream, char *grid) {
+  free(grid);
+  fclose(stream);
 }
 
-void
-	draw_shape(t_zone *zone, char *drawing, t_shape *shape)
-{
-	int	y;
-	int	x;
-	int	is_it;
+tile_t get_tile_status(float x, float y, float cx, float cy, float cradius) {
+  const float E = 1.000000;
+  const float dx = x - cx, dy = y - cy;
+  const float dist = sqrtf(powf(dx, 2.0) + powf(dy, 2.0));
 
-	y = 0;
-	while (y < zone->height)
-	{
-		x = 0;
-		while (x < zone->width)
-		{
-			is_it = in_circle((float)x, (float)y, shape);
-			if ((shape->type == 'c' && is_it == 2)
-				|| (shape->type == 'C' && is_it))
-				drawing[(y * zone->width) + x] = shape->color;
-			x++;
-		}
-		y++;
-	}
+  if (dist <= cradius) {
+    if (cradius - E < dist)
+      return EDGE;
+    return IN;
+  }
+  return OUT;
 }
 
-int
-	draw_shapes(FILE *file, t_zone *zone, char *drawing)
-{
-	t_shape	tmp;
-	int		ret;
+int main(int ac, char *av[]) {
+  FILE *stream;
+  int bg_width, bg_height;
+  char bg_char;
+  char *grid;
+  int res_len;
 
-	while ((ret = fscanf(file, "%c %f %f %f %c\n", &tmp.type, &tmp.x, &tmp.y, &tmp.radius, &tmp.color)) == 5)
-	{
-		if (tmp.radius <= 0.00000000 || (tmp.type != 'c' && tmp.type != 'C'))
-			return (0);
-		draw_shape(zone, drawing, &tmp);
-	}
-	if (ret != -1)
-		return (0);
-	return (1);
-}
+//args
+  if (ac != 2)
+    return str_err(ERR_ARG);
 
-void
-	draw_drawing(t_zone *zone, char *drawing)
-{
-	int	i;
+//grid_setup
+  if (!(stream = fopen(av[1], "r"))
+      || (fscanf(stream, "%d %d %c\n", &bg_width, &bg_height, &bg_char) != 3)
+      || (!(0 < bg_width && bg_width <= 300) || !(0 < bg_height && bg_height <= 300))
+      || (!(grid = malloc(bg_height * bg_width))))
+    return str_err(ERR_OP);
+  memset(grid, bg_char, bg_width * bg_height);
 
-	i = 0;
-	while (i < zone->height)
-	{
-		write(1, drawing + (i * zone->width), zone->width);
-		write(1, "\n", 1);
-		i++;
-	}
-}
+//grid_write
+  char ctype, cchar;
+  float cx, cy, cradius;
+  while ((res_len = fscanf(stream, "%c %f %f %f %c\n",
+          &ctype, &cx, &cy, &cradius, &cchar)) == 5) {
+    // error check
+    if (cradius <= ZERO || !(ctype == 'c' || ctype == 'C'))
+      break;
+    // write to grid
+    for (int y = 0; y < bg_height; y++) {
+      for (int x = 0; x < bg_width; x++) {
+        tile_t tile = get_tile_status(x, y, cx, cy, cradius);
+        if (tile == EDGE || (tile == IN && ctype == 'C'))
+          grid[y * bg_width + x] = cchar;
+      }
+    }
+  }
+  if (res_len != EOF) {
+    cleanup(stream, grid);
+    return str_err(ERR_OP);
+  }
 
-int
-	str_error(char const *str)
-{
-	if (str)
-		write(1, str, ft_strlen(str));
-	return (1);
-}
+// grid_print
+  for (int y = 0; y < bg_height; y++) {
+    write(1, grid + y * bg_width, bg_width);
+    write(1, "\n", 1);
+  }
 
-int
-	clear_all(FILE *file, char *drawing, char const *str)
-{
-	if (file)
-		fclose(file);
-	if (drawing)
-		free(drawing);
-	if (str)
-		str_error(str);
-	return (1);
-}
-
-int
-	main(int argc, char **argv)
-{
-	FILE	*file;
-	t_zone	zone;
-	char	*drawing;
-
-	zone.width = 0;
-	zone.height = 0;
-	zone.background = 0;
-	drawing = NULL;
-	if (argc != 2)
-		return (str_error("Error: argument\n"));
-	if (!(file = fopen(argv[1], "r")))
-		return (str_error("Error: Operation file corrupted\n"));
-	if (!(drawing = get_zone(file, &zone)))
-		return (clear_all(file, NULL, "Error: Operation file corrupted\n"));
-	if (!(draw_shapes(file, &zone, drawing)))
-		return (clear_all(file, drawing, "Error: Operation file corrupted\n"));
-	draw_drawing(&zone, drawing);
-	clear_all(file, drawing, NULL);
-	return (0);
+//cleanup
+  cleanup(stream, grid);
+  return 0;
 }
